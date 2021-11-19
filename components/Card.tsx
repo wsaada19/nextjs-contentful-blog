cb35c04aa41fb1ab0423bad1708c43f4974e709d
wsaada19 <willsaada19@gmail.com>
2021-11-19 16:55:03 -0500

    fix issue with animation/text

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ type CardProps = {
 export const Card = ({ children, className = '' }: CardProps) => {
   return (
     <div
-      className={`${className} rounded-lg shadow-lg p-3 border border-gray-400 transform transition duration-300 hover:scale-1025`}
+      className={`${className} card rounded-lg shadow-lg p-3 border border-gray-400 transform transition duration-300 hover:scale-1025`}
     >
       {children}
     </div>
