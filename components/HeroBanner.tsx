b3212f291b98942661edc8300b124427561e1a7f
wsaada19 <willsaada19@gmail.com>
2021-11-28 20:38:18 -0500

    Update HeroBanner.tsx

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -16,7 +16,7 @@ export const HeroBanner = ({ title, image, summary }: HeroBannerProps) => {
       <Typography type="h1" className="mb-6 text-center">
         {title}
       </Typography>
-      {image.file.url && (
+      {image.file && (
         <Image
           src={`https://${image.file.url}`}
           alt={image.description}
