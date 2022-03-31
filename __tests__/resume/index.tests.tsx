837af6ca025379661ae303d3988579b013ac993f
wsaada19 <willsaada19@gmail.com>
2022-03-31 11:48:11 -0400

    fix build error and add first test!

---
+++ __tests__/resume/index.tests.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Resume from '../../pages/resume';
+describe('Resume', () => {
+  it('renders resume with my name', () => {
+    const { getByText } = render(<Resume />);
+
+    expect(getByText('Will Saada')).toBeInTheDocument();
+  });
+});
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { Switch } from './Switch';
+import { Switch } from './switch';
 
 type NavigationProps = {
   hideLinks: boolean;
