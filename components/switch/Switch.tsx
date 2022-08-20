d77beb44a84048ac573e06893ef5ee0667e61182
wsaada19 <willsaada19@gmail.com>
2022-08-20 10:46:11 -0400

    fix switch id

--- components/switch/Switch.tsx
+++ components/switch/Switch.tsx
@@ -6,7 +6,7 @@ type SwitchProps = {
 
 export const Switch = ({ className }: SwitchProps) => {
   return (
-    <label className={className} htmlFor="toggleSwitch">
+    <label className={className} htmlFor="toggle-switch">
       <input
         className={`${styles.toggleSwitch} cursor-pointer rounded-full shadow-md appearance-none bg-gray-600 dark:bg-white h-6 w-12 border-1 border-blue-600 checked-bg-blue-600 transition duration-200 relative`}
         type="checkbox"
