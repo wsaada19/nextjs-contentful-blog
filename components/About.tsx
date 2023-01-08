38173de631f997e44949cf3519a4f90ad8659f31
wsaada19 <willsaada19@gmail.com>
2023-01-08 12:29:13 -0500

    change dark mode button color and convert file to js not json

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ type AboutProps = {
 
 export const About = ({ aboutText }: AboutProps) => {
   return (
-    <div className="bg-blue-700 shadow-xl border-2 border-blue-400 mt-3 mb-5 p-5 text-lg text-white transform transition duration-300 hover:scale-1025">
+    <div className="bg-blue-700 shadow-xl border-2 border-blue-400 mt-3 mb-5 p-3 text-lg text-white transform transition duration-300 hover:scale-1025 md:p-5">
       <p>{aboutText}</p>
     </div>
   );
--- services/nbaService/nbaClient.ts
+++ services/nbaService/nbaClient.ts
@@ -1,11 +1,13 @@
 import nba from 'nba-api-client';
-import teams from './nbaTeamEfficiency.json';
+import { teams } from './nbaTeamEfficiency';
 
 export async function getAdvancedTeamData() {
+  console.log('Getting team data!');
   const stats = await nba.leagueTeamGeneralStats({
     MeasureType: 'Advanced',
     Season: '2022-23',
   });
+  console.log('Team data received!');
   return teams.map((team, i) => {
     const teamAdvStats = stats.LeagueDashTeamStats[i];
     team.offRating = teamAdvStats.OFF_RATING;
