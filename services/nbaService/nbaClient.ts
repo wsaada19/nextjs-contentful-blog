09111c2eeced85dcb327fda5ecaa570e91ccc735
wsaada19 <willsaada19@gmail.com>
2023-01-08 12:37:03 -0500

    add try/catch for api call

--- services/nbaService/nbaClient.ts
+++ services/nbaService/nbaClient.ts
@@ -3,15 +3,20 @@ import { teams } from './nbaTeamEfficiency';
 
 export async function getAdvancedTeamData() {
   console.log('Getting team data!');
-  const stats = await nba.leagueTeamGeneralStats({
-    MeasureType: 'Advanced',
-    Season: '2022-23',
-  });
-  console.log('Team data received!');
-  return teams.map((team, i) => {
-    const teamAdvStats = stats.LeagueDashTeamStats[i];
-    team.offRating = teamAdvStats.OFF_RATING;
-    team.defRating = teamAdvStats.DEF_RATING;
-    return { ...team, wins: teamAdvStats.W };
-  });
+  try {
+    const stats = await nba.leagueTeamGeneralStats({
+      MeasureType: 'Advanced',
+      Season: '2022-23',
+    });
+    console.log('Team data received!');
+    return teams.map((team, i) => {
+      const teamAdvStats = stats.LeagueDashTeamStats[i];
+      team.offRating = teamAdvStats.OFF_RATING;
+      team.defRating = teamAdvStats.DEF_RATING;
+      return { ...team, wins: teamAdvStats.W };
+    });
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
