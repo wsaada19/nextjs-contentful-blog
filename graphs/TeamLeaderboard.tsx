255be8071dbac87c5424afe6f3d5b79960844a8f
wsaada19 <willsaada19@gmail.com>
2023-02-04 11:27:45 -0500

    Update TeamLeaderboard.tsx

--- graphs/TeamLeaderboard.tsx
+++ graphs/TeamLeaderboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TeamData } from 'types/nbaTeamData';
-import Image from "next/image";
+import Image from 'next/image';
 
 export type TeamLeaderBoardProps = {
   teams: TeamData[];
@@ -11,7 +11,7 @@ export const TeamLeaderBoard = ({ teams, className }: TeamLeaderBoardProps) => {
   return (
     <div className={`text-sm ${className}`}>
       <table className="text-white w-full">
-        <thead className="text-sm h-6 bg-blue-900 md:border md:border-gray-400 md:border-b-0">
+        <thead className="text-sm h-8 bg-blue-900 md:border md:border-gray-400 md:border-b-0">
           <tr>
             <th></th>
             <th></th>
@@ -26,21 +26,22 @@ export const TeamLeaderBoard = ({ teams, className }: TeamLeaderBoardProps) => {
           {teams.map((team, index) => {
             return (
               <tr
-                className=" text-black border border-gray-400 border-t-0 dark:bg-white even:bg-light dark:even:bg-light"
+                className="h-6 text-black border border-gray-400 border-t-0 dark:bg-white even:bg-light dark:even:bg-light"
                 key={team.name}
               >
                 <td className="pl-4 w-1/12">{index + 1}</td>
                 <td className="w-4">
-                  <div className="mt-2">
+                  <div>
                     <Image
                       src={team.logo}
                       height={24}
                       width={24}
                       alt={`${team.name} logo`}
                       style={{
-                        maxWidth: "100%",
-                        height: "auto"
-                      }}></Image>
+                        maxWidth: '100%',
+                        height: 'auto',
+                      }}
+                    ></Image>
                   </div>
                 </td>
                 <td className="w-10 font-semibold">{`${team.name}`}</td>
