63bd03bb0eb35dfb52fd763e678b07214205c90c
wsaada19 <willsaada19@gmail.com>
2023-04-12 10:23:44 -0400

    switch to vitest for testing

--- __tests__/resume/index.tests.tsx
+++ __tests__/resume/index.tests.tsx
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
 import Resume from '../../pages/resume';
+
 describe('Resume', () => {
   it('renders resume with my name', () => {
     const { getByText } = render(<Resume />);
---
+++ vitest.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test.setup.js'],
+    include: [
+      '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+      '__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    ],
+  },
+});
