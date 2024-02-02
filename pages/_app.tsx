acc9e9632277731d62c46d4c6c58181933edd426
wsaada19 <willsaada19@gmail.com>
2024-02-02 15:15:43 -0500

    update packagesss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
-
+const env = process.env.NODE_ENV;
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      {env == 'production' && <Analytics />}
     </>
   );
 }
