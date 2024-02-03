3fafc6eb6c9cd98628942346c567ac0b9965ce78
wsaada19 <willsaada19@gmail.com>
2024-02-02 20:57:13 -0500

    styling improvements

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,5 +1,5 @@
 import { ContentfulImage } from '@types';
-import Image from "next/image";
+import Image from 'next/image';
 import React from 'react';
 import { contentfulLoader } from '@utilities';
 
@@ -13,25 +13,28 @@ export const HeroBanner = ({ title, image, summary }: HeroBannerProps) => {
   const getImage = (): JSX.Element => {
     if (image.file) {
       const { width, height } = image.file.details.image;
-      return <>
-        <div className="mt-2">
-          <Image
-            src={`https://${image.file.url}`}
-            alt={image.description}
-            width={width}
-            height={height}
-            loader={contentfulLoader}
-            priority
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "auto"
-            }} />
-        </div>
-        <h2 className="text-sm font-normal mt-2 mb-3 text-black font-sans dark:text-white">
-          {summary}
-        </h2>
-      </>;
+      return (
+        <>
+          <div className="mt-4">
+            <Image
+              src={`https://${image.file.url}`}
+              alt={image.description}
+              width={width}
+              height={height}
+              loader={contentfulLoader}
+              priority
+              sizes="100vw"
+              style={{
+                width: '100%',
+                height: 'auto',
+              }}
+            />
+          </div>
+          <h2 className="text-sm font-normal mt-2 mb-3 text-black font-sans dark:text-white">
+            {summary}
+          </h2>
+        </>
+      );
     }
   };
   return (
